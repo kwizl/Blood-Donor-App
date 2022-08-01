dfb3fa4cf2028c51ec63f32f25f6f796e61fa1ff
martinn <martinn@innova.co.ke>
2022-08-01 22:27:55 +0300

    Fix bug in LoginActivity logic

--- app/src/main/java/com/example/blood_donor_app/LoginActivity.java
+++ app/src/main/java/com/example/blood_donor_app/LoginActivity.java
@@ -5,19 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private Button registerButton;
+    private TextView backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        registerButton = findViewById(R.id.registerButton);
+        backButton = findViewById(R.id.backButton);
 
-        registerButton.setOnClickListener(view -> {
+        backButton.setOnClickListener(view -> {
             Intent intent = new Intent(LoginActivity.this, SelectRegistrationActivity.class);
             startActivity(intent);
         });
